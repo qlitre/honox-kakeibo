05273dae4961ce6f856b93938b3c710198beb0c1
qlitre <kuri0403@gmail.com>
2025-03-22 10:17:19 +0900

    データ作成時のslack通知の実装

--- app/@types/dbTypes.ts
+++ app/@types/dbTypes.ts
@@ -1,100 +1,100 @@
 export type KakeiboBaseField = {
-    id: number
-    created_at: string;
-    updated_at: string
-}
-
+  id: number;
+  created_at: string;
+  updated_at: string;
+};
 
 export type KakeiboListResponse<T> = {
-    contents: T[];
-    totalCount: number;
-    limit: number;
-    offset: number;
-    pageSize: number;
-}
+  contents: T[];
+  totalCount: number;
+  limit: number;
+  offset: number;
+  pageSize: number;
+};
 
 export type AssetCategory = KakeiboBaseField & {
-    name: string;
-    is_investment: number; // sqliteのbool型は内部的に0か1となる
-}
+  name: string;
+  is_investment: number; // sqliteのbool型は内部的に0か1となる
+};
 
 export type Asset = KakeiboBaseField & {
-    date: string;
-    amount: number;
-    asset_category_id: number;
-    description?: string;
-}
+  date: string;
+  amount: number;
+  asset_category_id: number;
+  category_name: string;
+  description?: string;
+};
 
 export type AssetWithCategory = Asset & {
-    category_name: string;
-    is_investment: number; // sqliteのbool型は内部的に0か1となる
-}
-
+  category_name: string;
+  is_investment: number; // sqliteのbool型は内部的に0か1となる
+};
 
 export type FundTransation = KakeiboBaseField & {
-    date: string;
-    amount: number;
-    description?: string;
-}
-
-
+  date: string;
+  amount: number;
+  description?: string;
+};
 
 export type ExpenseCategory = KakeiboBaseField & {
-    name: string;
+  name: string;
 };
 
 export type PaymentMethod = KakeiboBaseField & {
-    name: string;
+  name: string;
 };
 
 export type Expense = KakeiboBaseField & {
-    date: string;
-    amount: number;
-    expense_category_id: number;
-    payment_method_id: number;
-    description?: string;
-}
+  date: string;
+  amount: number;
+  expense_category_id: number;
+  category_name: string;
+  payment_method_id: number;
+  payment_method_name: string;
+  description?: string;
+};
 
 export type IncomeCategory = KakeiboBaseField & {
-    name: string;
-}
+  name: string;
+};
 
 export type Income = KakeiboBaseField & {
-    date: string;
-    amount: number;
-    income_category_id: number;
-    description?: string;
-}
+  date: string;
+  amount: number;
+  income_category_id: number;
+  category_name: string;
+  description?: string;
+};
 
 export type IncomeWithCategory = Income & {
-    category_name: string
-}
+  category_name: string;
+};
 
 export type ExpenseWithDetails = Expense & {
-    category_name: string;
-    payment_method_name: string;
-}
-
+  category_name: string;
+  payment_method_name: string;
+};
 
 // 各サマリーデータの型定義
 export type SummaryItem = {
-    year_month: string;
-    total_amount: number;
-    category_name: string;
-    category_id: number;
+  year_month: string;
+  total_amount: number;
+  category_name: string;
+  category_id: number;
 };
 
 // SummaryResponseの型定義
 export type SummaryResponse = {
-    summary: SummaryItem[];
+  summary: SummaryItem[];
 };
 
-
-export type AssetWithCategoryResponse = KakeiboListResponse<AssetWithCategory>
-export type AssetCategoryResponse = KakeiboListResponse<AssetCategory>
-export type ExpenseWithDetailsResponse = KakeiboListResponse<ExpenseWithDetails>
-export type ExpenseCategoryResponse = KakeiboListResponse<ExpenseCategory>
-export type PaymentMethodResponse = KakeiboListResponse<PaymentMethod>
-export type IncomeCategoryResponse = KakeiboListResponse<IncomeCategory>
-export type IncomeWithCategoryResponse = KakeiboListResponse<IncomeWithCategory>
-export type FundTransationResponse = KakeiboListResponse<FundTransation>
+export type AssetWithCategoryResponse = KakeiboListResponse<AssetWithCategory>;
+export type AssetCategoryResponse = KakeiboListResponse<AssetCategory>;
+export type ExpenseWithDetailsResponse =
+  KakeiboListResponse<ExpenseWithDetails>;
+export type ExpenseCategoryResponse = KakeiboListResponse<ExpenseCategory>;
+export type PaymentMethodResponse = KakeiboListResponse<PaymentMethod>;
+export type IncomeCategoryResponse = KakeiboListResponse<IncomeCategory>;
+export type IncomeWithCategoryResponse =
+  KakeiboListResponse<IncomeWithCategory>;
+export type FundTransationResponse = KakeiboListResponse<FundTransation>;
--- app/routes/auth/income/create.tsx
+++ app/routes/auth/income/create.tsx
@@ -1,43 +1,64 @@
-import type { Income } from '@/@types/dbTypes';
-import { createRoute } from 'honox/factory'
-import { zValidator } from '@hono/zod-validator'
-import { z } from 'zod'
-import { KakeiboClient } from '@/libs/kakeiboClient';
-import { setCookie } from 'hono/cookie';
-import { alertCookieMaxage, successAlertCookieKey } from '@/settings/kakeiboSettings';
+import type { Income } from "@/@types/dbTypes";
+import { createRoute } from "honox/factory";
+import { zValidator } from "@hono/zod-validator";
+import { z } from "zod";
+import { KakeiboClient } from "@/libs/kakeiboClient";
+import { setCookie } from "hono/cookie";
+import {
+  alertCookieMaxage,
+  successAlertCookieKey,
+} from "@/settings/kakeiboSettings";
+import { sendSlackNotification } from "@/libs/slack";
 
 const schema = z.object({
-    date: z.string().length(10),
-    amount: z.string(),
-    income_category_id: z.string(),
-    description: z.string()
+  date: z.string().length(10),
+  amount: z.string(),
+  income_category_id: z.string(),
+  description: z.string(),
 });
 
 export const POST = createRoute(
-    zValidator('form', schema, async (result, c) => {
-        // 万が一失敗した時の考慮をどうするか。
-        if (!result.success) {
-            c.redirect('/auth/income', 303);
-        }
-    }),
-    async (c) => {
-        const client = new KakeiboClient({ token: c.env.HONO_IS_COOL, baseUrl: c.env.BASE_URL })
-        const { date, amount, income_category_id, description } = c.req.valid('form')
-        const parsedAmount = Number(amount);
-        const parsedCategoryId = Number(income_category_id);
-        if (isNaN(parsedAmount) || isNaN(parsedCategoryId)) {
-            return c.json({ error: 'Invalid number format' }, 400);
-        }
-        const body = {
-            date: date,
-            amount: parsedAmount,
-            income_category_id: parsedCategoryId,
-            description: description
-        }
-        const response = await client.addData<Income>({ endpoint: 'income', data: body })
-            .catch((e) => { console.error(e) })
-        if (response) {
-            setCookie(c, successAlertCookieKey, '収入追加に成功しました', { maxAge: alertCookieMaxage })
-            return c.redirect(`/auth/income?lastUpdate=${response.id}`, 303);
-        }
-    })
+  zValidator("form", schema, async (result, c) => {
+    // 万が一失敗した時の考慮をどうするか。
+    if (!result.success) {
+      c.redirect("/auth/income", 303);
+    }
+  }),
+  async (c) => {
+    const client = new KakeiboClient({
+      token: c.env.HONO_IS_COOL,
+      baseUrl: c.env.BASE_URL,
+    });
+    const { date, amount, income_category_id, description } =
+      c.req.valid("form");
+    const parsedAmount = Number(amount);
+    const parsedCategoryId = Number(income_category_id);
+    if (isNaN(parsedAmount) || isNaN(parsedCategoryId)) {
+      return c.json({ error: "Invalid number format" }, 400);
+    }
+    const body = {
+      date: date,
+      amount: parsedAmount,
+      income_category_id: parsedCategoryId,
+      description: description,
+    };
+    const response = await client
+      .addData<Income>({ endpoint: "income", data: body })
+      .catch((e) => {
+        console.error(e);
+      });
+    if (response) {
+      setCookie(c, successAlertCookieKey, "収入追加に成功しました", {
+        maxAge: alertCookieMaxage,
+      });
+      const message = `
+収入が追加されました。
+${response.date}
+カテゴリ:${response.category_name}
+金額：${response.amount}
+            `;
+      sendSlackNotification(message, c.env.SLACK_WEBHOOK_URL);
+      return c.redirect(`/auth/income?lastUpdate=${response.id}`, 303);
+    }
+  }
+);
