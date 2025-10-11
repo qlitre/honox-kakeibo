86926d74a77f9a4d30013cf767513f495cbcf294
qlitre <kuri0403@gmail.com>
2025-10-11 11:44:46 +0900

    chore

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,15 +1,15 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
-import path from 'path'
+import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
   test: {
     globals: true,
-    environment: 'jsdom',
+    environment: "jsdom",
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'app'),
+      "@": path.resolve(__dirname, "app"),
     },
   },
-})
+});
